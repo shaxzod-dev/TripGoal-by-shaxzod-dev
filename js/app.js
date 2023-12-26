9f3460e26b37520ac4a7ee56775183caa4f06c44
Shaxzodbek <ganijanovshaxzod9@gmail.com>
2023-12-26 13:42:03 +0300

    added js recursion

--- js/app.js
+++ js/app.js
@@ -73,7 +73,23 @@ var swiper = new Swiper(".mySwiper2", {
 //   console.log(num);
 //   num--;
 //   if (num >= 0) {
-//     test(num);
+//     `${test(num)}`
 //   }
 // }
-// test(10);
+
+// test(10)
+
+// rekursiya o'z ichida o'zi
+
+// for (let i = 0; i < 100; i++) {
+//   setTimeout (() => {
+//     console.log(i);
+//   }, 1000)
+// } 
+// set time out kutadi va qiladi
+
+// for (let i = 0; i < 2; i++) {
+//   setInterval(() => {
+//     console.log(i);
+//   }, 2000)
+// }  qayta qayta qiladi
